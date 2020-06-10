e9770f1dbe8a918026eedc443b839884f6779b8e
Björn Petersen <pheasn@gmail.com>
2020-06-10 19:18:48 +0200

    Fix some missing rounds

--- fix.py
+++ fix.py
@@ -1,18 +1,17 @@
 import re
 import json
 
-file = "tasks_unfucked.json"
+file = "tasks.json"
 
 with open(file, 'r', encoding='utf-8') as f:
     tasks = json.load(f)
 
 # TODO trink verteil
-keywords = ['hand', 'knie', 'bein', 'küssen', 'kuschel', 'schlägt',
-            'streichel', 'schlag auf', 'kneif', 'Backpfeife', 'tauscht', 'Nimm das Getränk']
+keywords = ['bis zum ende']
 regexes = [re.compile(f"\\s{keyword}(\\s|,)") for keyword in keywords]
 
 #TODO Getränkt
-excempts = [it.lower() for it in ['dein Knie mit deiner Stirn', 'die Hand heben', 'Handtasche','Wer sich als Letzter', 'Bilder auf deinem Handy', 'Schreibe jemandem den Satz', 'Getränkt','Kuschelrock', 'ob beim Furz nicht', 'Lieber ohne Beine', 'Handwerk', 'handstand', 'Handy', 'Handball', 'die luft an', "beinhalte", "Stimmt alle", "Hand trinken", "Links trinken", 'nur die linke Hand benutzen', 'Letzter auf dem Boden kniet', 'Kuscheltier', 'Verstecke etwas in deiner geschlossenen Faust']]
+excempts = [it.lower() for it in []]
 
 
 def is_excempt(lowertext: str) -> bool:
@@ -27,6 +26,14 @@ try:
             text: str = task['text']
             lowertext = text.lower()
 
+            if task['rounds'] == -1:
+                continue
+
+            for keyword in keywords:
+                if keyword in lowertext:
+                    task['rounds'] = -1
+                    break
+
 
 
 
@@ -34,7 +41,7 @@ except BaseException as e:
     print(e)
 
 
-outfile = 'tasks_unfucked.json'
+outfile = 'tasks.json'
 with open(outfile, 'w', encoding='utf-8') as f:
     outcontent = json.dumps(tasks, indent=4, ensure_ascii=False)
     f.write(outcontent)
