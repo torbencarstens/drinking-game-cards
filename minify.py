d36dbc5add6bdc5f1ec4d67e38712e97fce34d74
Torben Carstens <github@carstens.tech>
2020-07-01 18:54:07 +0200

    Add i18n
    Use `[tasks,minified]_{locale}.json` for filenames.
    e.g. tasks_de.json
    Applications should choose a default for this -> Don't add a
    `[tasks,minified].json`

--- minify.py
+++ minify.py
@@ -1,7 +1,15 @@
 import json
+import os
+import re
+
 
 with open("tasks.json") as input_file:
     tasks = json.load(input_file)
 
-with open("minified.json", "w+") as output_file:
-    json.dump(tasks, output_file, ensure_ascii=False)
+
+for file in os.listdir("."):
+    locale = re.findall(r"tasks_(\w{2}).json", file)
+    if locale:
+        with open(f"minified_{locale[0]}.json", "w+") as output_file:
+            json.dump(tasks, output_file, ensure_ascii=False)
+
