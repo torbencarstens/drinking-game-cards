05aa12e358837deec0557afee73bae4f0e117fc2
Torben Carstens <github@carstens.tech>
2020-08-02 22:37:48 +0200

    Update some english tasks

--- migrate.py
+++ migrate.py
@@ -2,16 +2,12 @@ import json
 import string
 
 
-with open("tasks.json") as input_file:
+with open("tasks_en.json") as input_file:
     tasks = json.load(input_file)
 
 for task in tasks:
-    replacement_variables = string.ascii_lowercase[8:8 + task.get("text").count("{int}")]
-    task["variables"] = {}
+    text = "Drink {int: "
+    task["text"] = task["text"].replace("Drink {int: ", "Everybody drink {int: ")
 
-    for rv in replacement_variables:
-        task["text"] = task.get("text").replace("{int}", f"{{int: {rv}}}", 1)
-        task["variables"][rv] = "ANY"
-
-with open("tasks.json", "w+") as output_file:
+with open("tasks_en.json", "w+") as output_file:
     json.dump(tasks, output_file, ensure_ascii=False, indent=2)
